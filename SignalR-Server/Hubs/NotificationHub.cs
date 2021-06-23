289fb980161eeee3ecf0fde181dae8393bf0cce2
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-23 21:40:57 +0300

    SignalR-Server ASP.Net Core Web MVC

---
+++ SignalR-Server/Hubs/NotificationHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Server.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public Task SendMessage(string message)
+        {
+            return Clients.Others.SendAsync("Send", message);
+        }
+    }
+}
